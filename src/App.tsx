bf9ff51673974b69144b7c37cb296814a8d421c6
김진범 <thepolaris@jei.com>
2023-07-04 14:46:10 +0900

    update useApi: add suspense&enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
 import { Suspense } from 'react';
 import { IconContext } from 'react-icons';
 
-// Create a client
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: { suspense: true, refetchOnWindowFocus: false, retry: false },
-        mutations: { retry: false }
-    }
-});
-
 export default function App() {
     return (
         <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <IconContext.Provider value={{ className: 'w-6 h-6' }}>
-                        <Router />
-                    </IconContext.Provider>
-                </Suspense>
-            </QueryClientProvider>
+            <Suspense fallback={<div>Suspense Loading...</div>}>
+                <IconContext.Provider value={{ className: 'w-6 h-6' }}>
+                    <Router />
+                </IconContext.Provider>
+            </Suspense>
         </BrowserRouter>
     );
 }
