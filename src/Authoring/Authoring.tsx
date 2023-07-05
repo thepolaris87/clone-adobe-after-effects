4a0a1b8a99e534ec26a18474401782af14e5e7ad
jiaezzang <visveryver2@gmail.com>
2023-07-05 17:18:42 +0900

    left, right content 생성

--- src/Authoring/Authoring.tsx
+++ src/Authoring/Authoring.tsx
@@ -1,6 +1,8 @@
 import Toolbox from './Toolbox/Toolbox';
 import Viewer from './Viewer/Viewer';
 import { Transform } from './Footer/Transform';
+import LeftContent from './SideContents/LeftContent';
+import RightContent from './SideContents/RightContent';
 
 export default function Authoring() {
     return (
@@ -8,11 +10,15 @@ export default function Authoring() {
             <Toolbox />
             <hr className="border-cyan-950"></hr>
             <div className="flex">
-                <div className="flex-[1]">Left Content</div>
+                <div className="flex-[1]">
+                    <LeftContent />
+                </div>
                 <div>
                     <Viewer />
                 </div>
-                <div className="flex-[1.5]">Right Content</div>
+                <div className="flex-[1.5]">
+                    <RightContent />
+                </div>
             </div>
             <hr className="border-cyan-950"></hr>
             <div className="flex-[1] overflow-auto p-2">
---
+++ src/Authoring/SideContents/RightContent.tsx
@@ -0,0 +1,5 @@
+export default function RightContent() {
+  return (
+    <div>RightContent</div>
+  )
+}
