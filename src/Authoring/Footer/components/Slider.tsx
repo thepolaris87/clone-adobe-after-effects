87da46d4a8068a1525ce48d88f362ba6c60198c5
1seul357 <1seul357@gmail.com>
2023-07-06 13:05:21 +0900

    에러 수정

--- src/Authoring/Footer/components/Slider.tsx
+++ src/Authoring/Footer/components/Slider.tsx
@@ -6,10 +6,16 @@ export const Slider = ({ timeMinValue, timeMaxValue, setTimeMaxValue, setTimeMin
     const activeObj = useAtomValue(activeObjectAtom);
 
     return (
-        <div className={'hidden sm:flex w-[55%] items-center relative'} style={{ backgroundColor: objectId === activeObj.data.id ? '#dddbdb' : '#ecebeb' }}>
+        <div
+            className={'hidden sm:flex w-[55%] items-center relative'}
+            style={{ backgroundColor: activeObj && objectId === activeObj.data.id ? '#dddbdb' : '#ecebeb' }}
+        >
             <Input value={timeMinValue} onCheck={onCheckRange} setValue={setTimeMinValue} />
             <Input value={timeMaxValue} onCheck={onCheckRange} setValue={setTimeMaxValue} />
-            <div className="relative h-[7px] w-full rounded-[4px]" style={{ backgroundColor: objectId === activeObj.data.id ? '#ecebeb' : '#dddddd' }}>
+            <div
+                className="relative h-[7px] w-full rounded-[4px]"
+                style={{ backgroundColor: activeObj && objectId === activeObj.data.id ? '#ecebeb' : '#dddddd' }}
+            >
                 <div className="absolute rounded-[10px] h-[7px] bg-[#b0b0b0]" style={{ left: timeMinValue + '%', right: 100 - timeMaxValue + '%' }} />
             </div>
         </div>
