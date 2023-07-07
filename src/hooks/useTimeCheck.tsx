bbe0f8a4d2b10c8a0c448e62bdf09537287a53a3
1seul357 <1seul357@gmail.com>
2023-07-07 15:27:14 +0900

    hooks 추가

---
+++ src/hooks/useTimeCheck.tsx
@@ -0,0 +1,28 @@
+import { useCallback, useState, useEffect, useRef } from 'react';
+
+export const useTimeCheck = () => {
+    const [time, setTime] = useState(0);
+    const interval = useRef<ReturnType<typeof setInterval>>();
+
+    const start = useCallback(() => {
+        interval.current = setInterval(() => {
+            setTime((prev) => {
+                const currentTime = prev + 1;
+                return currentTime;
+            });
+        }, 1000);
+    }, []);
+
+    const stop = () => {
+        setTime(0);
+        clearInterval(interval.current);
+    };
+
+    useEffect(() => {
+        return () => {
+            clearInterval(interval.current);
+        };
+    }, []);
+
+    return { time, start, stop };
+};
