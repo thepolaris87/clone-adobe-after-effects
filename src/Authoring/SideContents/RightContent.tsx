d1f8deabcb3f2223826bf34d26aa8e40cef5e055
jiaezzang <visveryver2@gmail.com>
2023-07-07 09:01:00 +0900

    RigthContent 형식

--- src/Authoring/SideContents/RightContent.tsx
+++ src/Authoring/SideContents/RightContent.tsx
@@ -1,5 +1,66 @@
 export default function RightContent() {
-  return (
-    <div>RightContent</div>
-  )
+    return (
+        <>
+            <div>
+                <div className="font-extrabold pt-3">Transform</div>
+                <div>
+                    <div>Position</div>
+                    <div className="flex gap-3">
+                        <div className="flex flex-col">
+                            <label>Center-X</label>
+                            <input type="text" className="w-16"></input>
+                        </div>
+                        <div className="flex flex-col">
+                            <label>Center-Y</label>
+                            <input type="text" className="w-16"></input>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>Size</div>
+                    <div>
+                        <div className="flex gap-3">
+                            <div className="flex flex-col">
+                                <label>Width</label>
+                                <input type="text" className="w-16"></input>
+                            </div>
+                            <div className="flex flex-col">
+                                <label>Height</label>
+                                <input type="text" className="w-16"></input>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex gap-3">
+                                <div className="flex flex-col">
+                                    <label>Scale-X</label>
+                                    <input type="text" className="w-16"></input>
+                                </div>
+                                <div className="flex flex-col">
+                                    <label>Scale-Y</label>
+                                    <input type="text" className="w-16"></input>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>Rotate</div>
+                    <div className="flex flex-col">
+                        <label>Angle</label>
+                        <input type="text" className="w-16"></input>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="font-extrabold pt-6">Attribute</div>
+                <div>
+                    <div>Opacity</div>
+                    <div className="flex flex-col">
+                        <label>Angle</label>
+                        <input type="text" className="w-16"></input>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
