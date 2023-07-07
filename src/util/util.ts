f2b449fed7f0ab86a8cf6b631fd44f5a5ca3d86e
1seul357 <1seul357@gmail.com>
2023-07-07 15:27:23 +0900

    util 함수 추가

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,10 @@
+export const wait = (delay: number) =>
+    new Promise((resolve: (value?: unknown) => void) =>
+        setTimeout(() => {
+            resolve();
+        }, delay)
+    );
+
 export const sound = (src: string) => {
     const audio = new Audio();
     audio.src = src;
@@ -11,4 +18,12 @@ export const sound = (src: string) => {
     };
 
     return { audio, play, stop };
+};
+
+export const onSetTimeLine = ({ object, id, timeMinValue, timeMaxValue }: TimeLineProps) => {
+    const effects = object.data.effects.map((effect: EffectProps, index: number) => {
+        if (index === id) return { ...effect, timeLine: [timeMinValue, timeMaxValue] };
+        return effect;
+    });
+    object.set('data', { ...object.get('data'), effects });
 };
