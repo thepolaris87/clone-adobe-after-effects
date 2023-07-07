1c43f5b1b338638450ca4592513f970ef22ba945
jiaezzang <visveryver2@gmail.com>
2023-07-07 16:34:08 +0900

    컴포넌트 수정

--- src/Authoring/SideContents/RightContent.tsx
+++ src/Authoring/SideContents/RightContent.tsx
@@ -1,32 +1,78 @@
 import { useAtomValue } from 'jotai';
 import Input from './Components/Input';
 import { activeObjectAtom, editorAtom } from '@/atoms/atom';
-import { useEffect, useState } from 'react';
-
+import { MouseEventHandler, useEffect, useState } from 'react';
+type position = {
+    centerX: number | undefined;
+    centerY: number | undefined;
+}
+type size = {
+    width: number | undefined;
+    height: number | undefined;
+}
+type scale = {
+    scaleX: number | undefined;
+    scaleY: number | undefined;
+}
+type setting = {
+    angle: number | undefined;
+    opacity: number | undefined;
+}
 export default function RightContent() {
     const editor = useAtomValue(editorAtom);
     const canvas = editor?.canvas;
     const activeObject: any = useAtomValue(activeObjectAtom);
-    const [position, setPosition] = useState({ centerX: '', centerY: '' });
-    const [size, setSize] = useState({ width: '', height: '' });
-    const [scaleValue, setScaleValue] = useState({ scaleX: '', scaleY: '' });
-    const [setting, setSetting] = useState({ angle: '', opacity: '' });
+    const [position, setPosition] = useState<position>({ centerX: undefined, centerY: undefined });
+    const [size, setSize] = useState<size>({ width: undefined, height: undefined });
+    const [scaleValue, setScaleValue] = useState<scale>({ scaleX: undefined, scaleY: undefined });
+    const [setting, setSetting] = useState<setting>({ angle: undefined, opacity: undefined });
 
     //초기화
     useEffect(() => {
         if (Object.keys(activeObject).length === 0) {
-            setPosition({ centerX: '', centerY: '' });
-            setSize({ width: '', height: '' });
-            setScaleValue({ scaleX: '', scaleY: '' });
-            setSetting({ angle: '', opacity: '' });
+            setPosition({ centerX: undefined, centerY: undefined });
+            setSize({ width: undefined, height: undefined });
+            setScaleValue({ scaleX: undefined, scaleY: undefined });
+            setSetting({ angle: undefined, opacity: undefined });
             return;
         }
         setPosition({ centerX: activeObject.left, centerY: activeObject.top });
         setSize({ width: activeObject.width, height: activeObject.height });
         setScaleValue({ scaleX: activeObject.scaleX, scaleY: activeObject.scaleY });
-        activeObject.angle ? setSetting((prev) => ({ ...prev, angle: activeObject.angle })) : setSetting((prev) => ({ ...prev, angle: '0' }));
+        activeObject.angle ? setSetting((prev) => ({ ...prev, angle: activeObject.angle })) : setSetting((prev) => ({ ...prev, angle: 0 }));
         setSetting((prev) => ({ ...prev, opacity: activeObject.opacity }));
     }, [activeObject]);
+
+    useEffect(() => {
+        const handleObjectModified = (e: any) => {
+            setSetting((prev) => ({ ...prev, opacity: e.target.opacity }));
+        };
+        const handleObjectMoving = (e: fabric.IEvent) => {
+            setPosition({ centerX: e.target?.left, centerY: e.target?.top });
+        };
+        const handleObjectScaling = (e: any) => {
+            setScaleValue({ scaleX: e.target.scaleX, scaleY: e.target.scaleY });
+        };
+
+        const handleObjectRotating = (e: any) => {
+            setSetting((prev) => ({ ...prev, angle: e.target.angle }));
+        };
+
+        if (canvas) {
+            canvas.on('object:modified', handleObjectModified);
+            canvas.on('object:moving', handleObjectMoving);
+            canvas.on('object:scaling', handleObjectScaling);
+            canvas.on('object:rotating', handleObjectRotating);
+        }
+
+        return () => {
+            if (canvas) {
+                canvas.off('object:moving', handleObjectMoving);
+                canvas.off('object:scaling', handleObjectScaling);
+                canvas.off('object:rotating', handleObjectRotating);
+            }
+        };
+    }, [canvas]);
 
     //Position
     const inputCenter = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,30 +80,24 @@ export default function RightContent() {
         canvas?.renderAll();
 
         if (e.target.name === 'centerX') {
-            setPosition((prev) => ({ ...prev, centerX: e.target.value }));
+            setPosition((prev) => ({ ...prev, centerX: Number(e.target.value) }));
             activeObject.set({ left: Number(e.target.value), top: activeObject.top });
         } else if (e.target.name === 'centerY') {
-            setPosition((prev) => ({ ...prev, centerY: e.target.value }));
+            setPosition((prev) => ({ ...prev, centerY: Number(e.target.value) }));
             activeObject.set({ left: activeObject.left, top: Number(e.target.value) });
         }
         canvas?.renderAll();
     };
 
     //Size
-    const inputSize = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.value) activeObject.set({ width: Number(e.target.value), height: activeObject.height });
-        canvas?.renderAll();
-        console.log(activeObject.width);
-    };
-
     const inputScale = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (Object.keys(activeObject).length === 0) return;
 
         if (e.target.name === 'scaleX') {
-            setScaleValue((prev) => ({ ...prev, scaleX: e.target.value }));
+            setScaleValue((prev) => ({ ...prev, scaleX: Number(e.target.value) }));
             activeObject.set({ scaleX: e.target.value, sclaeY: activeObject.scaleY });
         } else if (e.target.name === 'scaleY') {
-            setScaleValue((prev) => ({ ...prev, scaleY: e.target.value }));
+            setScaleValue((prev) => ({ ...prev, scaleY: Number(e.target.value) }));
             activeObject.set({ scaleX: activeObject.scaleX, scaleY: e.target.value });
         }
         canvas?.renderAll();
@@ -67,14 +107,14 @@ export default function RightContent() {
     const inputAngle = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (Object.keys(activeObject).length === 0) return;
 
-        setSetting((prev) => ({ ...prev, angle: e.target.value }));
+        setSetting((prev) => ({ ...prev, angle: Number(e.target.value) }));
         activeObject.rotate(e.target.value);
         canvas?.renderAll();
     };
 
     const inputOpacity = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (Object.keys(activeObject).length === 0) return;
-        setSetting((prev) => ({ ...prev, opacity: e.target.value }));
+        setSetting((prev) => ({ ...prev, opacity: Number(e.target.value) }));
         activeObject.set({ opacity: e.target.value });
         canvas?.renderAll();
     };
@@ -88,13 +128,13 @@ export default function RightContent() {
                         <Input
                             title="Center-X"
                             name="centerX"
-                            value={position.centerX || ''}
+                            value={position.centerX}
                             onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputCenter(e)}
                         />
                         <Input
                             title="Center-Y"
                             name="centerY"
-                            value={position.centerY || ''}
+                            value={position.centerY}
                             onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputCenter(e)}
                         />
                     </div>
@@ -103,22 +143,22 @@ export default function RightContent() {
                     <div>Size</div>
                     <div>
                         <div className="flex gap-3">
-                            <Input title="Width" value={size.width || ''} readOnly={true} />
+                            <Input title="Width" value={size.width} readOnly={true} />
 
-                            <Input title="Height" value={size.height || ''} readOnly={true} />
+                            <Input title="Height" value={size.height} readOnly={true} />
                         </div>
                         <div>
                             <div className="flex gap-3">
                                 <Input
                                     title="Scale-X"
                                     name="scaleX"
-                                    value={scaleValue.scaleX || ''}
+                                    value={scaleValue.scaleX}
                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputScale(e)}
                                 />
                                 <Input
                                     title="Scale-Y"
                                     name="scaleY"
-                                    value={scaleValue.scaleY || ''}
+                                    value={scaleValue.scaleY}
                                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputScale(e)}
                                 />
                             </div>
@@ -127,13 +167,13 @@ export default function RightContent() {
                 </div>
                 <div className="border-b pb-3 w-40">
                     <div>Rotate</div>
-                    <Input title="Angle" value={setting.angle || ''} onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputAngle(e)} />
+                    <Input title="Angle" value={setting.angle} onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputAngle(e)} />
                 </div>
             </div>
             <div>
                 <div className="font-extrabold pt-6">Attribute</div>
                 <div>
-                    <Input title="Opacity" value={setting.opacity || ''} onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputOpacity(e)} />
+                    <Input title="Opacity" value={setting.opacity} onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputOpacity(e)} />
                 </div>
             </div>
         </>
