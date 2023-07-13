84cae809d30f414f12b05d421f454a990e6ab521
jiaezzang <visveryver2@gmail.com>
2023-07-13 09:49:20 +0900

    그룹해제

--- src/Authoring/Toolbox/Group.tsx
+++ src/Authoring/Toolbox/Group.tsx
@@ -2,9 +2,10 @@ import fabric from '@fabric';
 import { activeObjectAtom, editorAtom, objectsAtom } from '@/atoms/atom';
 import { IconButtonV1 } from '@/components/Button';
 import { useAtomValue, useSetAtom } from 'jotai';
-import { LiaObjectGroup } from 'react-icons/lia';
+import { LiaObjectGroup, LiaObjectUngroup } from 'react-icons/lia';
 import { nanoid } from 'nanoid';
 import { indexAtom } from './atom';
+import { useEffect } from 'react';
 
 export default function Group() {
     const editor = useAtomValue(editorAtom);
@@ -14,26 +15,33 @@ export default function Group() {
     const setIndexClick = useSetAtom(indexAtom);
 
     const onGroupBtnClick = () => {
-        if (!(activeObject as fabric.Object).data && (activeObject as any)?._objects?.length > 1) {
+        if (!(activeObject as fabric.Object)?.data && (activeObject as any)?._objects?.length > 1) {
+            const group = new fabric.Group((activeObject as any)?._objects, { left: activeObject?.left, top: activeObject?.top });
             setIndexClick(true);
             //그룹화
-            const group = new fabric.Group((activeObject as any)._objects, { left: activeObject?.left, top: activeObject?.top });
             group.set('data', { type: 'group', id: nanoid(), effects: [] });
             const objects = editor?.add(group);
             if (objects) setObjects(objects);
 
-            //기존 객체 삭제
-            (activeObject as any)._objects.forEach(function (obj: fabric.Object) {
+            //기존 개체 삭제
+            (activeObject as any)._objects.forEach((obj: fabric.Object) => {
                 canvas?.remove(obj);
             });
+
+            canvas?.renderAll();
+        } else if ((activeObject as fabric.Object)?.data?.type === 'group') {
+            if (!activeObject) return;
+            setIndexClick(true);
+
+            //그룹해제
+            (activeObject as any).toActiveSelection();
+            canvas?.renderAll();
         }
     };
 
     return (
         <div>
-            <IconButtonV1 onClick={onGroupBtnClick}>
-                <LiaObjectGroup />
-            </IconButtonV1>
+            <IconButtonV1 onClick={onGroupBtnClick}>{activeObject?.data?.type === 'group' ? <LiaObjectUngroup /> : <LiaObjectGroup />}</IconButtonV1>
         </div>
     );
 }
