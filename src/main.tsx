a77a696573d338cfce97b111cfee1706c0674947
jiaezzang <visveryver2@gmail.com>
2023-07-14 15:39:30 +0900

    폰트 로드

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './assets/styles';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    // <React.StrictMode>
-    <App />
-    // </React.StrictMode>,
-);
+const preloadFont = (font: string) =>
+    new Promise((resolve) => {
+        document.fonts.load(font).then(resolve);
+    });
+
+const fontList = ['30px NotoSansKRRegular', '30px NanumSquereRoundR', '30px NanumSquereRoundB', '30px OneMobilePOP'];
+
+Promise.all([fontList.forEach((el) => preloadFont(el))]).then(() => {
+    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+        // <React.StrictMode>
+        <App />
+        // </React.StrictMode>,
+    );
+});
