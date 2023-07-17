9c37f4e9d380fc707273d4780051d07fca37380e
1seul357 <1seul357@gmail.com>
2023-07-17 17:14:19 +0900

    keyboard align 기능 추가

---
+++ src/hooks/useKeyboardEvent/useAlign.ts
@@ -0,0 +1,16 @@
+import Editor from '@/Editor/Editor';
+
+export default function useAlign() {
+    const onAlign = (editor: Editor) => {
+        const objects = editor.canvas.getActiveObject();
+        if (!objects) return;
+
+        (objects as fabric.Group)._objects.forEach((object) => {
+            const bounding = object.getBoundingRect();
+            object.set('left', 0 - bounding.width / 2);
+            object.set('top', 0 - bounding.height / 2);
+            editor.canvas.renderAll();
+        });
+    };
+    return { onAlign };
+}
